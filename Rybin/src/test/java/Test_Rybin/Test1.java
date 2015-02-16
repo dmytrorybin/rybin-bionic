e50c9409b713937b3eeb1d450c3f9ac03c87e874
Dmytro Rybin <dmytro.rybin@gmail.com>
2015-02-16 20:02:13 +0200

    16.03 Test1.lava v1.1

--- Rybin/src/test/java/Test_Rybin/Test1.java
+++ Rybin/src/test/java/Test_Rybin/Test1.java
@@ -13,28 +13,28 @@ import java.util.Scanner;
 
 public class Test1 {
 
+                     // Чтение из файла
     @Test
-                                 // Чтение из файла
     public void T1() {
         FileReader fr = null;
         try {
             fr = new FileReader("C:\\_git\\file1.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        Scanner scan = new Scanner(fr);
-        String string = "";
-        while (scan.hasNextLine()) {
-            string = scan.nextLine();
-        }
-        Assert.assertEquals(string, "abc123");
+    }
+    Scanner scan = new Scanner(fr);
+    String string = "";
+    while (scan.hasNextLine()) {
+        string = scan.nextLine();
+    }
+    Assert.assertEquals(string, "abc123");
     }
 
                             // проверка на real
     @Test
     public void T2() {
-        String var = "11.6";
-        Assert.assertTrue(var.matches("^\\d+(\\.\\d+)?"));
+        String var = "89789789.9898654";
+        Assert.assertTrue(var.matches("^\\d+(\\.\\d+)"));
     }
 }
 
